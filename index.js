95cd8f1e5731d4e8f0574dc1b1ad37773cb99b76
Chaitanya Santosh Tekane <chaitanyatekne5@gmail.com>
2021-08-07 15:22:24 +0530

    Add Styles

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const readlineSync= require("readline-sync");
+const chalk= require("chalk");
 
 const monthDays = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 let testFlag = false;
 
-const userName = readlineSync.question("Welcome, Can You Please Enter Your Name?\n");
-console.log(`Hello ${userName}, Now I will tell you if your birth date is a prime number or not!!\n`);
+const userName = readlineSync.question(chalk.blueBright("Welcome, Can You Please Enter Your Name?\n"));
+console.log(chalk.yellow(`Hello ${userName}, Now I will tell you if your birth date is a prime number or not!!\n`));
 
 takeDate();
 
 function takeDate(){
-    const inputDate = readlineSync.question("Please enter your birth-date in the format 'DD/MM'\n");
+    const inputDate = readlineSync.question(chalk.blueBright("Please enter your birth-date in the format 'DD/MM'\n"));
 
     const dateArray = inputDate.split('/');
     const DD = dateArray[0];
@@ -17,13 +18,13 @@ function takeDate(){
 
     // check if date is valid or not
     if(isNaN(MM) || isNaN(DD) || (!Number.isInteger(Number(MM))) || (!Number.isInteger(Number(DD)))){
-        console.log("\nYou Entered Invalid Date");
+        console.log(chalk.redBright("\nYou Entered Invalid Date"));
         takeDate();
     }else if(MM<=0 || DD<=0 || MM>12 || DD>31){
-        console.log("\nYou Entered Invalid Date");
+        console.log(chalk.redBright("\nYou Entered Invalid Date"));
         takeDate();
     }else if(DD>monthDays[MM-1]){
-        console.log("\nYou Entered Invalid Date");
+        console.log(chalk.redBright("\nYou Entered Invalid Date"));
         takeDate();
     }else{
         isPrime(DD);
@@ -35,7 +36,7 @@ function isPrime(testNo){
     testNo = Number(testNo);
 
     if(testNo===1){
-        console.log("\nYour Birth-Date Is Neither Prime Nor Composite");
+        console.log(chalk.yellowBright("\nYour Birth-Date Is Neither Prime Nor Composite"));
     }else if(testNo===2){
         testFlag = false;
     }else{
@@ -50,7 +51,7 @@ function isPrime(testNo){
 
 // print final result
 if(testFlag){
-    console.log("\nOops, Your Birth-Date Is Not A Prime Number");
+    console.log(chalk.yellowBright("\nOops, Your Birth-Date Is Not A Prime Number"));
 }else{
-    console.log("\nAwesome, Your Birth-Date Is A Prime Number");
+    console.log(chalk.yellowBright("\nAwesome, Your Birth-Date Is A Prime Number"));
 }
